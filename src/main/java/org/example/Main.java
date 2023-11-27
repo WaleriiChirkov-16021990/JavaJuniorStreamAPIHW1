9d791dc3733a456a4c7434b39f52adeb204adf11
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-11-27 16:52:42 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SuppressWarnings("EndlessStream")
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * 0.1. Посмотреть разные статьи на Хабр.ру про Stream API
+         * +
+         * 0.2. Посмотреть видеоролики на YouTube.com Тагира Валеева про Stream API
+         * +
+         *
+         * 1. Создать список из 1_000 рандомных чисел от 1 до 1_000_000
+         *
+         * 1.1 Найти максимальное
+         * 2.2 Все числа, большие, чем 500_000, умножить на 5, отнять от них 150 и просуммировать
+         * 2.3 Найти количество чисел, квадрат которых меньше, чем 100_000
+         *
+         * 2. Создать класс Employee (Сотрудник) с полями: String name, int age, double salary, String department
+         * 2.1 Создать список из 10-20 сотрудников
+         * 2.2 Вывести список всех различных отделов (department) по списку сотрудников
+         * 2.3 Всем сотрудникам, чья зарплата меньше 10_000, повысить зарплату на 20%
+         * 2.4 * Из списка сотрудников с помощью стрима создать Map<String, List<Employee>> с отделами и сотрудниками внутри отдела
+         * 2.5 * Из списока сорудников с помощью стрима создать Map<String, Double> с отделами и средней зарплатой внутри отдела
+         */
+
+//        1. Создать список из 1_000 рандомных чисел от 1 до 1_000_000
+
+        List<Integer> randomNumbers = ThreadLocalRandom.current().ints(1, 1000000).boxed().limit(1000).collect(Collectors.toList());
+        System.out.println(randomNumbers);
+
+//        Все числа, большие, чем 500_000, умножить на 5, отнять от них 150 и просуммировать
+        int sumElement = 0;
+//        randomNumbers.stream().filter(i -> i > 500000).map(i -> ((i * 5) - 150)).mapToInt(Integer::sum).close();
+    }
+}
